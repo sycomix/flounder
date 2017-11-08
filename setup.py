d5f51ba90414a6f3a73ab729a824d709ce6225f3
Kyohei Tsuda <shimano.entou@gmail.com>
2017-11-08 23:17:18 +0900

    Add a setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+requires = ["requests==2.18.4"]
+
+
+setup(
+    name='flounder',
+    version='0.0.1',
+    description='flounder is a library that create Entity of Dialogflow. This library uses RestAPI of dialogflow. It is not an official library.',
+    url='https://github.com/flatfisher/flounder',
+    author='flatfisher',
+    author_email='shimano.entou@gmail.com',
+    license='MIT',
+    keywords='dialogflow', 'entity', 'dialogflow entity'],
+    packages=[
+        "flounder"
+    ],
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 2.7.13',
+    ],
+)
