fba8f4945b2662101a829a36920de147250a16e6
Kyohei Tsuda <shimano.entou@gmail.com>
2017-11-08 23:17:46 +0900

    Add a project dir

---
+++ flounder/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    flounder
+    ~~~~~~~~
+    :copyright: (c) 2017 by flatfisher.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import
+
+__version__ = "0.0.1"
+__license__ = "MIT"
