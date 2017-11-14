aff3096b9c030e6021119c931a095e26d2634a3b
Kyohei Tsuda <k-tsuda@colorful-board.com>
2017-11-14 14:32:44 +0900

    Add client_access_token

--- flounder/flounder.py
+++ flounder/flounder.py
@@ -1,1 +1,25 @@
-print ("coming soon")
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+
+class Flounder(object):
+
+    @property
+    def client_access_token(self):
+        """
+            Client access token provided by https://dialogflow.com/
+            :rtype: str or unicode
+        """
+
+        return self._client_access_token
+
+    @client_access_token.setter
+    def client_access_token(self, client_access_token):
+        """
+            :type client_access_token: str or unicode
+        """
+
+        self._client_access_token = client_access_token
+
+    def __init__(self, client_access_token):
+        super(Flounder, self).__init__()
+        self._client_access_token = client_access_token
