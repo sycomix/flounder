0628566862432069855f0f030916e2dd5f0e01bb
Kyohei Tsuda <shimano.entou@gmail.com>
2017-11-15 00:09:06 +0900

    Add a test code

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import unittest
+
+from .test_actions import *
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_entity_upload.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+import os
+
+try:
+    from flounder.flounder import Flounder
+except ImportError:
+    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+    from flounder.flounder import Flounder
+
+CLIENT_ACCESS_TOKEN = 'CLIENT_ACCESS_TOKEN'
+
+class TestActions(unittest.TestCase):
+    def setUp(self):
+        self.flounder = Flounder(CLIENT_ACCESS_TOKEN)
+
+    def test_show_token(self):
+        print self.flounder.client_access_token
+
+if __name__ == '__main__':
+    unittest.main()
