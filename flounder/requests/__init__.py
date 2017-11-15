a88adaebb643b9ea9b5fc1bb421ae3f15da6f6b8
Kyohei Tsuda <k-tsuda@colorful-board.com>
2017-11-15 10:01:41 +0900

    create a few new files, unimplemented

---
+++ flounder/requests/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+
+from .request import Request
+from .entity import CreateRequest
+
+"""
+dialogflow
+~~~~~~~~~~~~~~~~
+This module provides a Dialogflow classes to manage requests.
+"""
+
+__all__ = [
+    'Request',
+    'CreateRequest'
+]
---
+++ flounder/requests/entity/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+
+from .entity import CreateRequest
+
+
+"""
+dialogflow
+~~~~~~~~~~~~~~~~
+This module provides a dialogflow classes to manage requests.
+"""
+
+__all__ = [
+    'CreateRequest'
+]
